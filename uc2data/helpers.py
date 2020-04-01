c9c76faa99d61928f3d852a12c031586f29e0922
holtmann <achim.holtmann@tu-berlin.de>
2020-04-01 12:55:26 +0200

    do overwrite check result in multi check

--- uc2data/helpers.py
+++ uc2data/helpers.py
@@ -8,15 +8,13 @@ def check_multi(folder):
     for path in pathlist:
         outfile = Path(str(path).replace(".nc", ".check"))
 
-        if not outfile.exists():
+        try:
+            i_data = Dataset(path)
+            i_data.uc2_check()
+            print_me = str(i_data.check_result.errors) + str(i_data.check_result.warnings)
+        except Exception:
+            print_me = "Could not read file: "+str(path)
 
-            try:
-                i_data = Dataset(path)
-                i_data.uc2_check()
-                print_me = str(i_data.check_result.errors) + str(i_data.check_result.warnings)
-            except Exception:
-                print_me = "Could not read file: "+str(path)
-
-            text_file = open(str(outfile), "w")
-            text_file.write(print_me)
-            text_file.close()
+        text_file = open(str(outfile), "w")
+        text_file.write(print_me)
+        text_file.close()
