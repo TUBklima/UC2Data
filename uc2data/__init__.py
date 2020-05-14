7a8d2b853a59ea1752c01423cc807a3f309f50a1
holtmann <achim.holtmann@tu-berlin.de>
2020-05-14 13:13:52 +0200

    new data standard version 1.4.1

--- uc2data/__init__.py
+++ uc2data/__init__.py
@@ -2,4 +2,4 @@ from .Dataset import Dataset
 from .Result import ResultCode, ResultItem, CheckResult
 from .helpers import check_multi
 
-data_standard_version = (1,4)
+data_standard_version = (1,4,1)
